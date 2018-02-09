52559a32ce158e1bcf2049efcf04c3cd884d63d3
braden <haskiezgaming@gmail.com>
2018-02-08 20:30:02 -0700

    Add api call in main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log($(document));
+$(document).ready(function() {
+    $.ajax({
+        url: "proxy.php",
+        type: "POST",
+        data: {
+            address: "http://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0"
+        },
+        success: function(r) {
+            console.log(r);
+        }
+    })
+});
