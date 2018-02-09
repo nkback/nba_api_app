3c9d8c6f22a1eb7f8af50c773355a32ef83d1ee3
braden <haskiezgaming@gmail.com>
2018-02-08 20:37:10 -0700

    delete proxy.php change to get

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-        url: "proxy.php",
-        type: "POST",
-        data: {
-            address: "http://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0"
-        },
+        url: "http://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
+        type: "GET",
         success: function(r) {
             console.log(r);
         }
