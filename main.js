99b23731f1bbbab51c4ed154b995bb03b1a4bbc2
braden <haskiezgaming@gmail.com>
2018-02-08 20:44:34 -0700

    cors

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
+        url: "https://cors.io/?u=https://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
         type: "GET",
         success: function(r) {
             console.log(r);
