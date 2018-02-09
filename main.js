16e8a571651541d8490d44de869b79d6109b7b76
braden <haskiezgaming@gmail.com>
2018-02-08 20:39:23 -0700

    Add secure

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
+        url: "https://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
         type: "GET",
         success: function(r) {
             console.log(r);
