0c4ab7dccb9ed5b63918a169235116c36debf95d
braden <haskiezgaming@gmail.com>
2018-02-16 13:53:41 -0700

    Might fix CORS error

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "https://cors.io/?u=https://stats.nba.com/stats/scoreboard/?GameDate=02/08/2018&LeagueID=00&DayOffset=0",
-        type: "GET",
-        success: function(r) {
-            console.log(r);
+    function nbaApiHit(url, callback) {
+        var request = new XMLHttpRequest();
+
+        request.addEventListener("load", onLoad);
+        request.addEventListener("error", onFail);
+        request.addEventListener("abort", onFail);
+
+        request.open("GET", url);
+        request.send();
+
+        function onLoad(e) {
+            if (request.status >= 400) {
+                onFail(e);
+            }
+            else {
+                var data = JSON.parse(this.responseText);
+                callback(null, data);
+            }
+        }
+
+        function onFail(e) {
+            callback(new Error("Something went horribly wrong"));
+        }
+    }
+
+
+    // Hit the api
+    nbaApiHit("http://stats.nba.com/stats/scoreboard/?GameDate=02/16/2018&LeagueID=00&DayOffset=0", function(error, data) {
+        if (error) {
+            console.log("There was an error", error);
+        }
+        else {
+            console.log("Data is ", data);
         }
     })
 });
