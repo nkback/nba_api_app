c82d2b4244440b50b50f9de2dc76c7bba3f9cf2f
braden <haskiezgaming@gmail.com>
2018-03-21 12:23:57 -0600

    commit

--- main.js
+++ main.js
@@ -1,37 +1,11 @@
 $(document).ready(function() {
-    function nbaApiHit(url, callback) {
-        var request = new XMLHttpRequest();
-
-        request.addEventListener("load", onLoad);
-        request.addEventListener("error", onFail);
-        request.addEventListener("abort", onFail);
-
-        request.open("GET", url);
-        request.send();
-
-        function onLoad(e) {
-            if (request.status >= 400) {
-                onFail(e);
-            }
-            else {
-                var data = JSON.parse(this.responseText);
-                callback(null, data);
-            }
-        }
-
-        function onFail(e) {
-            callback(new Error("Something went horribly wrong"));
-        }
-    }
-
-
-    // Hit the api
-    nbaApiHit("https://stats.nba.com/stats/scoreboard/?GameDate=02/16/2018&LeagueID=00&DayOffset=0", function(error, data) {
-        if (error) {
-            console.log("There was an error", error);
-        }
-        else {
-            console.log("Data is ", data);
-        }
+    var url = "https://stats.nba.com/stats/scoreboard/?GameDate=02/16/2018&LeagueID=00&DayOffset=0";
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data){
+        console.log(data);
     })
+    .catch(function(error){
+        alert("Error");
+    });
 });
